f3a3536f448fbc26c261259380a306b4360f0418
Oluwatobi Adelaja <randomsandstrings@gmail.com>
2026-09-17 22:02:58 +0100

    added test for availability

--- app/api/availability/page.ts
+++ app/api/availability/page.ts
@@ -1,5 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
-export default function GET(){
+export async function GET(){
     return NextResponse.json({ready:"ok"},{status:200});
 }
